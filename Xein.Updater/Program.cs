d8397b350cd7a5afd1c5af3bacbf2ac9d33e2829
NekoMeow <nopeople123456789@gmail.com>
2021-06-19 22:54:35 +0800

    Add Updater

---
+++ Xein.Updater/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Runtime.InteropServices;
+
+#pragma warning disable IDE0060 // Remove unused parameter
+
+namespace Xein.Updater
+{
+    class Program
+    {
+        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+        public static extern int MessageBox(IntPtr hwnd, string message, string title, int flag);
+
+        static void ShowMsg(string msg, string title = "Error")
+        {
+            _ = MessageBox((IntPtr)0, msg, title, 0);
+        }
+
+        static void Main(string[] args)
+        {
+            if (!File.Exists("Update.zip"))
+            {
+                ShowMsg("No Update File Found. Exiting.");
+            }
+            else
+            {
+                try
+                {
+                    ZipFile.ExtractToDirectory("Update.zip", "./", true);
+                    ShowMsg("Update Successfully.");
+                }
+                catch (Exception e)
+                {
+                    ShowMsg($"Message:\n{e.Message}\nStack Trace:\n{e.StackTrace}", "Exception Triggered");
+                }
+            }
+        }
+    }
+}
